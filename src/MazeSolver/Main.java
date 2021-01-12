6a71bf5fb39d83c56435f21acb35f77f38b41c22
stefanbertici <69802287+stefanbertici@users.noreply.github.com>
2021-01-12 13:09:49 +0200

    added a little recursive method in BFS

--- src/MazeSolver/Main.java
+++ src/MazeSolver/Main.java
@@ -11,27 +11,29 @@ public class Main {
         //1 = available path
         //2 = exit
         int[][] maze = {
-                {1,1,1,1,1,1},
+                {1,1,1,2,1,1},
                 {1,0,1,0,0,1},
                 {1,1,1,1,0,1},
                 {1,1,1,1,1,1},
                 {1,0,1,0,1,1},
-                {1,1,1,1,2,1}
+                {1,1,1,1,0,1}
         };
 
         //we define our starting point
-        Point start = new Point(0, 5);
+        Point startPoint = new Point(0, 5);
 
-        depthFirstSearch(maze, start);
+        depthFirstSearch(maze, startPoint);
         System.out.println("\n");
-        breathFirstSearch(maze, start);
+        breathFirstSearch(maze, startPoint);
     }
 
+    //test maze to check and modify instead of original maze
     public static int[][] initializeTestMaze(int[][] maze) {
-        //test maze to check and modify instead of original maze
         int[][] testMaze = new int[maze.length][maze[0].length];
-        for (int i = 0; i < maze.length; i++)
+
+        for (int i = 0; i < maze.length; i++) {
             System.arraycopy(maze[i], 0, testMaze[i], 0, maze[0].length);
+        }
 
         return testMaze;
     }
@@ -43,48 +45,42 @@ public class Main {
     public static void printPath(Stack<Point> path, boolean isDFS) {
         if (isDFS) {
             System.out.println("You won!\nThe path to the exit using DFS (depth first search) is:");
-            for (int i = 0; i < path.size(); i++) {
-                if (i < path.size() - 1) {
-                    System.out.print(path.get(i) + " -> ");
-                } else {
-                    System.out.print(path.get(i));
-                }
-            }
         } else {
             System.out.println("You won!\nThe path to the exit using BFS (breath first search) is:");
-            for (int i = path.size() - 1; i >= 0; i--) {
-                if (i > 0) {
-                    System.out.print(path.get(i) + " -> ");
-                } else {
-                    System.out.print(path.get(i));
-                }
+        }
+
+        for (int i = 0; i < path.size(); i++) {
+            if (i < path.size() - 1) {
+                System.out.print(path.get(i) + " -> ");
+            } else {
+                System.out.print(path.get(i));
             }
         }
     }
 
-    public static void depthFirstSearch(int[][] givenMaze, Point start) {
+    public static void depthFirstSearch(int[][] givenMaze, Point startPoint) {
         int[][] maze = initializeTestMaze(givenMaze);
         Stack<Point> path = new Stack<>();
-        path.push(start);
+        path.push(startPoint);
 
         //after we have set our starting point and added it to the path we'll go ahead and mark it with "0"
         //which means that we already visited this point
-        maze[start.getRow()][start.getCol()] = 0;
+        maze[startPoint.getRow()][startPoint.getCol()] = 0;
 
         while (true) {
             //we get the row and col values of the last point added to the path
             int row = path.peek().getRow();
             int col = path.peek().getCol();
 
-            //we try to go left and check for exit
-            if (isValid(row, col-1, maze) && maze[row][col-1] != 0) {
-                if (maze[row][col-1] == 2) {
-                    path.push(new Point(row, col-1));
+            //we try to go right and check for exit
+            if (isValid(row, col+1, maze) && maze[row][col+1] != 0) {
+                if (maze[row][col+1] == 2) {
+                    path.push(new Point(row, col+1));
                     printPath(path, true);
                     break;
                 } else {
-                    maze[row][col-1] = 0;
-                    path.push(new Point(row, col-1));
+                    maze[row][col+1] = 0;
+                    path.push(new Point(row, col+1));
                     continue;
                 }
             }
@@ -102,6 +98,19 @@ public class Main {
                 }
             }
 
+            //we try to go left and check for exit
+            if (isValid(row, col-1, maze) && maze[row][col-1] != 0) {
+                if (maze[row][col-1] == 2) {
+                    path.push(new Point(row, col-1));
+                    printPath(path, true);
+                    break;
+                } else {
+                    maze[row][col-1] = 0;
+                    path.push(new Point(row, col-1));
+                    continue;
+                }
+            }
+
             //we try to go up and check for exit
             if (isValid(row-1, col, maze) && maze[row-1][col] != 0) {
                 if (maze[row-1][col] == 2) {
@@ -111,19 +120,6 @@ public class Main {
                 } else {
                     maze[row-1][col] = 0;
                     path.push(new Point(row-1, col));
-                    continue;
-                }
-            }
-
-            //we try to go right and check for exit
-            if (isValid(row, col+1, maze) && maze[row][col+1] != 0) {
-                if (maze[row][col+1] == 2) {
-                    path.push(new Point(row, col+1));
-                    printPath(path, true);
-                    break;
-                } else {
-                    maze[row][col+1] = 0;
-                    path.push(new Point(row, col+1));
                     continue;
                 }
             }
@@ -140,15 +136,15 @@ public class Main {
         }
     }
 
-    public static void breathFirstSearch(int[][] givenMaze, Point start) {
+    public static void breathFirstSearch(int[][] givenMaze, Point startPoint) {
         int[][] maze = initializeTestMaze(givenMaze);
-        Point end = null;
+        Point endPoint = null;
         Queue<Point> queue = new LinkedList<>();
-        queue.add(start);
+        queue.add(startPoint);
 
         //after we have set our starting point and added it to the queue we'll go ahead and mark it with "0"
         //which means that we already visited this point
-        maze[start.getRow()][start.getCol()] = 0;
+        maze[startPoint.getRow()][startPoint.getCol()] = 0;
 
         while (!queue.isEmpty()) {
             //we get the row and col values of the first point in queue
@@ -161,7 +157,7 @@ public class Main {
             //right
             if (isValid(row, col+1, maze) && maze[row][col+1] != 0) {
                 if (maze[row][col+1] == 2) {
-                    end = new Point(row, col+1, point);
+                    endPoint = new Point(row, col+1, point);
                     break;
                 } else {
                     maze[row][col+1] = 0;
@@ -172,7 +168,7 @@ public class Main {
             //down
             if (isValid(row+1, col, maze) && maze[row+1][col] != 0) {
                 if (maze[row+1][col] == 2) {
-                    end = new Point(row+1, col, point);
+                    endPoint = new Point(row+1, col, point);
                     break;
                 } else {
                     maze[row+1][col] = 0;
@@ -183,7 +179,7 @@ public class Main {
             //left
             if (isValid(row, col-1, maze) && maze[row][col-1] != 0) {
                 if (maze[row][col-1] == 2) {
-                    end = new Point(row, col-1, point);
+                    endPoint = new Point(row, col-1, point);
                     break;
                 } else {
                     maze[row][col-1] = 0;
@@ -194,7 +190,7 @@ public class Main {
             //up
             if (isValid(row-1, col, maze) && maze[row-1][col] != 0) {
                 if (maze[row-1][col] == 2) {
-                    end = new Point(row-1, col, point);
+                    endPoint = new Point(row-1, col, point);
                     break;
                 } else {
                     maze[row-1][col] = 0;
@@ -206,15 +202,24 @@ public class Main {
         if (queue.isEmpty()) {
             System.out.println("there is no path to the exit :(");
         } else {
-            //we reconstruct the correct path starting from the endpoint going backwards with the getParent method
+            //we reconstruct the correct path starting from the endpoint going backwards with using
+            //recursion and the getParent method
             Stack<Point> path = new Stack<>();
-            path.push(end);
-            while (end.getParent() != null) {
-                path.push(end.getParent());
-                end = end.getParent();
-            }
-
+            addPointsInReverseOrder(path, endPoint);
             printPath(path, false);
         }
+    }
+
+    public static void addPointsInReverseOrder(Stack<Point> path, Point endPoint) {
+        if (endPoint == null) {
+            System.out.println("Error: endPoint is null!");
+            return;
+        }
+
+        if (endPoint.getParent() != null) {
+            addPointsInReverseOrder(path, endPoint.getParent());
+        }
+
+        path.push(endPoint);
     }
 }
